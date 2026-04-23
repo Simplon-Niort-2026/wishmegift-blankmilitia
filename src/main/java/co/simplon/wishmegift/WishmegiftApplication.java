8deda58a8c4dc7ac54c2c415134128eafa73a149
OraneM99 <orane10@live.fr>
2026-04-23 13:58:02 +0200

    fix: suppr duplicate dependency

--- src/main/java/co/simplon/wishmegift/WishmegiftApplication.java
+++ src/main/java/co/simplon/wishmegift/WishmegiftApplication.java
@@ -1,4 +1,4 @@
-package main.java.co.simplon.wishmegift;
+package co.simplon.wishmegift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
