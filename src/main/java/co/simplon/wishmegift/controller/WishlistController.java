24d9c972f9c29804e3b10b3cbd690415deb5725a
VANWIERST Julien <julien.vanwierst@externe.maif.fr>
2026-04-27 14:56:15 +0200

    feat: Wishlist

---
+++ src/main/java/co/simplon/wishmegift/controller/WishlistController.java
@@ -0,0 +1,43 @@
+package co.simplon.wishmegift.controller;
+
+import co.simplon.wishmegift.entity.Wishlist;
+
+import co.simplon.wishmegift.service.WishlistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wishlists")
+class WishlistController {
+
+    private final WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Wishlist> create(@RequestBody Wishlist wishlist) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(wishlistService.create(wishlist));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Wishlist>> getAll() {
+        return ResponseEntity.ok(wishlistService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Wishlist> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(wishlistService.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        wishlistService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/co/simplon/wishmegift/repository/WishlistRepository.java
@@ -0,0 +1,11 @@
+package co.simplon.wishmegift.repository;
+
+
+import co.simplon.wishmegift.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist, Long>{
+
+}
